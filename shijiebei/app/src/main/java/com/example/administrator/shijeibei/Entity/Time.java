a3d99acc8d7dc15267b1168ae937e6ed3af8ef49
Lhaiyu <1255759571@qq.com>
2018-06-07 17:04:04 +0800

    commit

---
+++ shijiebei/app/src/main/java/com/example/administrator/shijeibei/Entity/Time.java
@@ -0,0 +1,71 @@
+package com.example.administrator.shijeibei.Entity;
+
+/**
+ * Created by Administrator on 2018/6/7.
+ */
+
+public class Time {
+    private String time;
+    private String zhuchang;
+    private String kechang;
+    private int score1;
+    private int score2;
+    private int image1;
+    private int image2;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getZhuchang() {
+        return zhuchang;
+    }
+
+    public void setZhuchang(String zhuchang) {
+        this.zhuchang = zhuchang;
+    }
+
+    public String getKechang() {
+        return kechang;
+    }
+
+    public void setKechang(String kechang) {
+        this.kechang = kechang;
+    }
+
+    public int getScore1() {
+        return score1;
+    }
+
+    public void setScore1(int score1) {
+        this.score1 = score1;
+    }
+
+    public int getScore2() {
+        return score2;
+    }
+
+    public void setScore2(int score2) {
+        this.score2 = score2;
+    }
+
+    public int getImage1() {
+        return image1;
+    }
+
+    public void setImage1(int image1) {
+        this.image1 = image1;
+    }
+
+    public int getImage2() {
+        return image2;
+    }
+
+    public void setImage2(int image2) {
+        this.image2 = image2;
+    }
+}
