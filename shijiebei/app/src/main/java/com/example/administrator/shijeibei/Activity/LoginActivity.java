2fe52dcc91d94337ada55234af8551571d018711
wanghongwei <1172768786@qq.com>
2018-06-21 16:32:15 +0800

    登录逻辑

--- shijiebei/app/src/main/java/com/example/administrator/shijeibei/Activity/LoginActivity.java
+++ shijiebei/app/src/main/java/com/example/administrator/shijeibei/Activity/LoginActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -63,7 +64,7 @@ public class LoginActivity extends AppCompatActivity {
                 // (需指定参数的内容、参数的类型、编码方式）
                 //参数的内容
                 OkHttpClient okHttpClient=new OkHttpClient();
-                String username=edituserName.getText().toString().trim();
+                final String username=edituserName.getText().toString().trim();
                 String password=editpassword.getText().toString().trim();
                 if(TextUtils.isEmpty(username)||TextUtils.isEmpty(password)){
                     Looper.prepare();
@@ -91,7 +92,12 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onResponse(Call call, Response response) throws IOException {
                             if((response.body().string()).equals("loginsuccess")){
+                                String loginuser=username;
+//                                Looper.prepare();
+//                                Toast.makeText(LoginActivity.this,username,Toast.LENGTH_LONG).show();
+//                                Looper.loop();
                                 Intent intent=new Intent();
+                                intent.putExtra("loginuser",username);
                                 intent.setClass(LoginActivity.this,MainActivity.class);
                                 startActivity(intent);
 
